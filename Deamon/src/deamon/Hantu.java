7eb46c8b88edb9810e86a093b2d9e0017712edb8
faksi <faksi121315@gmail.com>
2018-10-22 14:26:50 +0700

    nice

---
+++ Deamon/src/deamon/Hantu.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deamon;
+
+/**
+ *
+ * @author MHF
+ */
+public class Hantu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public void bersuara() {
+
+        System.out.println("hihihihihi");
+
+    }
+
+    public void terbang() {
+
+        System.out.println("Terbang 90 ");
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
