e39dd8e5c0a0f6091cad830ac368c3f2ee829c52
Faksi <faksi121315@gmail.com>
2018-12-17 21:30:44 +0700

    OKE

---
+++ LatihanMVC/src/edu/FaksiKilatBuanaArifinPutra/latihanmvc/controller/PelangganController.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.FaksiKilatBuanaArifinPutra.latihanmvc.controller;
+
+import edu.FaksiKilatBuanaArifinPutra.latihanmvc.model.PelangganModel;
+import edu.FaksiKilatBuanaArifinPutra.latihanmvc.view.PelangganView;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author MHF
+ * NIM   : 10117112
+ * Nama  : Faksi Kilat Buana Arifin Putra
+ * Kelas : IF-3
+ */
+public class PelangganController {
+    
+    private PelangganModel model;
+
+    public void setModel(PelangganModel model) {
+        this.model = model;
+    }
+    
+    
+    
+    public void resetForm(PelangganView view){
+        
+        String nama = view.getTxtNama().getText();
+        String email = view.getTxtEmail().getText();
+        String noTlp = view.getTxtTelp().getText();
+        
+        if (nama.equals("") && email.equals("") && noTlp.equals("")){
+            
+        }else {
+            
+            model.resetForm();
+        }
+        
+    }
+    
+    public void simpanForm(PelangganView view){
+        
+        String nama = view.getTxtNama().getText();
+        String email = view.getTxtEmail().getText();
+        String noTlp = view.getTxtTelp().getText();
+        
+        if (nama.trim().equals("")){
+            JOptionPane.showMessageDialog(view,"Nama Masih Kosong");
+        }else if(email.trim().equals("")){
+            JOptionPane.showMessageDialog(view,"Email Masih Kosong");
+        }else if(noTlp.trim().equals("")){
+            JOptionPane.showMessageDialog(view,"No Telepon Masih Kosong");
+        }else {
+            model.simpanForm();
+        }
+    }
+    
+}
---
+++ LatihanMVC/src/edu/FaksiKilatBuanaArifinPutra/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.FaksiKilatBuanaArifinPutra.latihanmvc.event;
+
+import edu.FaksiKilatBuanaArifinPutra.latihanmvc.model.PelangganModel;
+
+/**
+ *
+ * @author MHF
+ * NIM   : 10117112
+ * Nama  : Faksi Kilat Buana Arifin Putra
+ * Kelas : IF-3
+ */
+public interface PelangganListener {
+    
+    public void onChange (PelangganModel pelangan);
+}
